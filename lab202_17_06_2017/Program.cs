d39ada690210207feb63b3167d3429413c6e606d
lala97 <lalamamedova97@gmail.com>
2017-06-17 13:07:25 +0400

    add class

---
+++ lab202_17_06_2017/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab202_17_06_2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+         //   int b = Convert.ToInt32(Console.ReadLine());
+
+            p202 my_class_name = new p202();
+            //   my_class_name.sum(a, b);
+            double result = my_class_name.maas(a);
+            Console.WriteLine(result);
+            
+
+
+            Console.ReadKey();
+           
+          
+
+            //if (5>3)
+            //{
+            //    Console.WriteLine("duzdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("sehvdir");
+            //}
+        }
+    }
+}
---
+++ lab202_17_06_2017/p202.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab202_17_06_2017
+{
+    class p202
+    {
+        public int sum(int a,int b)
+        {
+            int sum = 0;
+
+            for (int i = a; i <= b; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("sum=" + sum);
+
+            return sum;
+        }
+
+        public double maas(double ayliq_gelir)
+
+        {
+            if (ayliq_gelir > 1000)
+            {
+                ayliq_gelir -= ayliq_gelir * 0.1;
+
+            }
+            else
+            {
+                ayliq_gelir -= ayliq_gelir * 0.05;
+            }
+            return ayliq_gelir;
+        }
+    }
+}
